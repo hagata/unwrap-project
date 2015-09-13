818fa3e0c3f4bbc5c42c30cb54653b7f9dff13bf
hagata <genki@hagata.info>
2015-09-13 13:07:37 -0700

    Sets up folder/files for list command adds commander

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 #! /usr/bin/env node
 
+var unwrap = require('commander');
+var fs = require('fs');
+var exec = require('child_process').exec;
+var storage = require('./utils/storage');
+
+
 console.log('Setup complete');
+
+unwrap
+  .command('list')
+  .alias('ls')
+  .description('List all projects that are saved in Unwrap-Projects')
+  .option('-d, --directories', 'show the root directories of each project in the list')
+  .action(function() {
+    storage.list();
+  })
+
+  unwrap.parse(process.argv);
